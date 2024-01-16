4abeb3022404104ce713e9393cfee704cfbc7ba9
Julen Martinez <julen.martinez@ikasle.egibide.org>
2024-01-16 20:57:40 +0100

    add route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/InicioSesion', function () {
+    return "Ruta con nombre!";
+})->name('InicioSesion.index');
