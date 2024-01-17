87e47db2ad677f1bb9dd5579b9b954cedbe41aec
Julen Martinez <julen.martinez@ikasle.egibide.org>
2024-01-17 09:57:54 +0100

    Edit css + hrml login, add images in resources,

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,20 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    @vite(['resources/css/styles.css', 'resources/js/app.js'])
 </head>
 <body>
     <div class="login-container">
-        <h1>Login</h1>
-        <form>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-            
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
-            
-            <button type="submit">Login</button>
+        <form class="login-form">
+          <h1>Egibide Netflix</h1>
+          <p>Porfavor entre en su cuenta</p>
+          <div class="input-group">
+            <input type="text" id="username" name="username" placeholder="Username" required>
+          </div>
+          <div class="input-group">
+            <input type="password" id="password" name="password" placeholder="Password" required>
+          </div>
+          <button type="submit">Login</button>
         </form>
-    </div>
+      </div>
 </body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,16 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        rollupOptions: {
+            // Copiar la carpeta images a public/build
+            plugins: [
+                require('rollup-plugin-copy')({
+                    targets: [
+                        { src: 'resources/images', dest: 'public/build' },
+                    ],
+                }),
+            ],
+        },
+    },
 });
