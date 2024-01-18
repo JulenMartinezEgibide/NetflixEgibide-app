c6da41575d7185fb26a6426ff161a9e8d2db272d
Julen Martinez <julen.martinez@ikasle.egibide.org>
2024-01-18 12:50:04 +0100

    Create controllers Admin,Alumno,Auth(login) + Model Usuario + Migration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AlumnoController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +20,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
+// Ruta para mostrar el formulario de inicio de sesión
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+
+// Ruta para procesar la autenticación
+Route::post('/login', [AuthController::class, 'login']);
+
+// Rutas para el administrador
+Route::group(['middleware' => ['auth', 'admin']], function () {
+    Route::get('/admin', [AdminController::class, 'showIndex'])->name('admin.index');
+});
+
+// Rutas para el alumno
+Route::group(['middleware' => ['auth', 'alumno']], function () {
+    Route::get('/alumno',  [AlumnoController::class, 'showIndex'])->name('alumno.index');
+});
