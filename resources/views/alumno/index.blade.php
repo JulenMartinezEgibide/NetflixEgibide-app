ba719008010da15aaeec6f2d7ade0bf0494e03ca
Julen Martinez <julen.martinez@ikasle.egibide.org>
2024-01-22 12:44:28 +0100

    creating general nabvar and test

--- resources/views/alumno/index.blade.php
+++ resources/views/alumno/index.blade.php
@@ -7,6 +7,13 @@
     @vite(['resources/css/styles.css', 'resources/js/app.js'])
 </head>
 <body>
+    <nav class="navbar">
+        <ul class="navbar-menu">
+          <li class="navbar-item"><a href="#" class="navbar-link">Peliculas</a></li>
+          <li class="navbar-item"><a href="#" class="navbar-link">Series</a></li>
+          <li class="navbar-item navbar-item-right"><a href="#" class="navbar-link">Cerrar sesion</a></li>
+        </ul>
+      </nav>
     <div class="login-container">
         <form class="login-form">
           <h1>HOLA ALUMNO</h1>
